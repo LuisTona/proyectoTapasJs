e7735bc45f7f87553a9b6daaa989fc76805f9645
Adrian Gavela Rodriguez <adriangavela09@gmail.com>
2025-01-12 23:47:03 +0100

    insertar/a/medias

--- js/insertar.js
+++ js/insertar.js
@@ -1,7 +1,39 @@
-// import { logOut } from "./landingPage.js";
 import { controlUsuarios } from "./comprobacionUser.js";
+import { dataBares } from "./data.js";
 
 
 controlUsuarios();
+
+let formulario = document.getElementById('formulario');
+let nombreBar = document.getElementById('nombreBar');
+let nombreTapa = document.getElementById('nombreTapa');
+let imagen = document.getElementById('imagen');
+let descripcion = document.getElementById('descripcion');
+
+dataBares.forEach(element => {
+    console.log(element);
+    let arrayId = element.tapas;
+    arrayId.forEach(elementTapa =>{
+        console.log(elementTapa[0]);
+    })
+});
+
+formulario.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    let dataTapa = {
+        nombreTapa: nombreBar.value.trim(),
+        tapa: [
+            {
+                id: null,
+                nombreTapa: nombreTapa.value.trim(),
+                imagenTapa: imagen.value.trim(),
+                descricion: descripcion.value.trim(),
+            }
+        ]
+    };
+    dataBares.push(dataTapa);
+    console.log(dataBares);
+})
+
 
 
